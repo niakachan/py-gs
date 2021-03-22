9167ae4cfbb7047d3970ae6134c252511e082c34
niakachan <niaka@miraitasu.jp>
2021-03-23 05:47:51 +0900

    Fix to create secrets directory when it does not exist.

--- settings/env.py
+++ settings/env.py
@@ -6,13 +6,18 @@ from dotenv import load_dotenv
 import dotenv
 
 
+def get_secret_dir():
+    path = os.environ.get('SECRETS_DIRECTORY') or './secrets'
+    os.makedirs(path, exist_ok=True)
+    return path
+
+
 # .envをロード
 dotenv_path = dotenv.find_dotenv()
 load_dotenv(dotenv_path, verbose=True)
 
 # GOOGLE_JSON_KEY を置いとくディレクトリ
-JSON_KEY_DIRECTORY = os.environ.get('SECRETS_DIRECTORY')
+JSON_KEY_DIRECTORY = get_secret_dir()
 
 # .envからの読み込み項目設定
-GOOGLE_JSON_KEY = '{0}/{1}.json'.format(JSON_KEY_DIRECTORY, os.environ.get('GOOGLE_JSON_KEY'))
-BOOK_ID = os.environ.get('BOOK_ID')
+GOOGLE_JSON_KEY = '{0}/{1}.json'.format(JSON_KEY_DIRECTORY, os.environ.get('GOOGLE_JSON_KEY')) or 'key.json'
