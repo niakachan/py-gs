bb08b8f6240e89182f7f5f061767c8f7212a6602
niakachan <niaka@miraitasu.jp>
2021-05-07 16:54:07 +0900

    Update to unify the response format.

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from functions import ss
 BOOK_URL = 'https://docs.google.com/spreadsheets/d/1sEt1V9mXT2CM7h94AgV4se3hZQlc2HzZ_ha01bzGuc0'
 
 # URLから取得したスプレッドシートを`book`に代入
-book = ss.get_book(BOOK_URL)
+response = ss.get_book(BOOK_URL)
+book = response['data']
+print(response)
 
 # `book`の中の 'Sheet1' というシートを sheet に代入
-sheet = ss.get_sheet(book, 'Sheet1')
-print(sheet)
+response = ss.get_sheet(book, 'Sheet1')
+sheet = response['data']
+print(response)
 
-# `sheet` の内容を dict形式で取得する
-sheet_dict = ss.get_dict(sheet)
-print(sheet_dict)
+# `sheet` の内容を dictのリスト形式で取得する
+response = ss.get_records(sheet)
+sheet_records = response['data']
+print(response)
 
 # `sheet` の中から `10` を含むセルを検索
-response = ss.search_all_cells(sheet, 10)
+response = ss.get_all_cells(sheet, 10)
 found_cells = response['data']
 print(response)
 
@@ -31,18 +35,23 @@ print(response)
 
 # 行番号からセルのリストを取得する
 row = 7
-row_cells = ss.get_row_cells(sheet, row)
-print(row_cells)
+response = ss.get_row_cells(sheet, row)
+row_cells = response['data']
+print(response)
 
 # セルのリストを値のリストに変換する
-cells_values = ss.get_cells_values(row_cells)
-print(cells_values)
+response = ss.get_cells_values(row_cells)
+cells_values = response['data']
+print(response)
 
 # 列番号から1行目を無視してセルのリストを取得する
 col = 4
-col_cells = ss.get_col_cells(sheet, col, 1)
-print(col_cells)
+ignore = 1
+response = ss.get_col_cells(sheet, col, ignore)
+col_cells = response['data']
+print(response)
 
 # セルのリストを値のリストに変換する
-cells_values = ss.get_cells_values(col_cells)
-print(cells_values)
+response = ss.get_cells_values(col_cells)
+cells_values = response['data']
+print(response)
